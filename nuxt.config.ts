5d104508fc139251cc308a80dc1456d6d0ee2634
shaozhilin <502121489@qq.com>
2025-05-14 17:05:41 +0800

    更新 nuxt.config.ts，确保服务器端渲染 (SSR) 功能启用

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase", "@nuxt/ui"],
   css: ["~/assets/css/main.css"],
+  ssr:true,
   ui: {
     theme: {
       colors: ["gray"],
