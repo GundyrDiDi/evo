a79325c052bfa2e2b9e3beb9ffaf8db93ed66caa
shaozhilin <502121489@qq.com>
2025-05-14 17:36:49 +0800

    更新登录页，添加 GitHub OAuth 登录重定向选项

--- pages/login.vue
+++ pages/login.vue
@@ -5,17 +5,19 @@ const { auth } = useSupabaseClient()
 if (user.value) {
   navigateTo('/home')
 }
+
+const r = location.origin
 </script>
 
 <template>
-  
+
   <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <h2 class="my-6 text-center text-3xl font-extrabold">
       Sign in to your account
     </h2>
     <LoginCard>
       <UButton class="mt-3" icon="i-mdi-github" block label="Github" color="primary" variant="solid"
-        @click="auth.signInWithOAuth({ provider: 'github' })" />
+        @click="auth.signInWithOAuth({ provider: 'github', options: { redirectTo: r } })" />
     </LoginCard>
   </div>
 </template>
