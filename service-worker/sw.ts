5de8c2997df5fee08e21bad23620a7b88dc99fa2
shaozhilin <502121489@qq.com>
2025-08-20 16:13:31 +0800

    feat:sw.js

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,36 +1,30 @@
-// import { clientsClaim } from 'workbox-core'
+import { clientsClaim } from 'workbox-core'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
 
-// console.log(clientsClaim)
-// import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-// import { NavigationRoute, registerRoute } from 'workbox-routing'
+declare let self: ServiceWorkerGlobalScope
 
-// declare let self: ServiceWorkerGlobalScope
-
-// // self.__WB_MANIFEST is default injection point
-// precacheAndRoute(self.__WB_MANIFEST)
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
 
 // // clean old assets
-// cleanupOutdatedCaches()
+cleanupOutdatedCaches()
 
-// let allowlist: undefined | RegExp[]
-// if (import.meta.env.DEV)
-//   allowlist = [/^\/$/]
+let allowlist: undefined | RegExp[]
+if (import.meta.env.DEV)
+  allowlist = [/^\/$/]
 
-// // to allow work offline
-// registerRoute(new NavigationRoute(
-//   createHandlerBoundToURL('/'),
-//   { allowlist },
-// ))
+// to allow work offline
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('/'),
+  { allowlist },
+))
 
-// self.skipWaiting()
-// clientsClaim()
+self.skipWaiting()
+clientsClaim()
 
 // Service Worker 核心能力
 // 离线资源缓存
 // 拦截和处理网络请求
 // 后台数据同步
 // 推送通知管理
-
-// const db = indexedDB.open("a");
-
-// console.log({db})
